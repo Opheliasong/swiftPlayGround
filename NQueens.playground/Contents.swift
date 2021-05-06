ecb0d7fe09610b288210e5ae44af9f110d02669a
SungMin Park <ophelia.song00@gmail.com>
2021-05-06 23:23:00 +0900

    Add N-Queens problem playbground -N-Queens 문제 플레이그라운드 1차 커밋 -decision space의 looping의 최적화가 덜 되어 있다. -1차 알고리즘 구현에 대한 아이디얼한 생각 구현

---
+++ NQueens.playground/Contents.swift
@@ -0,0 +1,79 @@
+import UIKit
+/***
+ N-Queens Problem
+ N x N의 체스보드에 N개의 퀸말을 놓는데, 퀸말들 사이에 얼마나 퀸을 배치할 수 있는가?
+ 퀸의 공격 범위에 간섭을 받으면 안된다
+ - 같은 행, 같은 열, 대각선상에 서로 배치될 수 없다
+ */
+let size:Int = 4
+var xPosition:[Int] = [Int](repeating: Int.min, count: size)
+var yPosition:[Int] = [Int](repeating: Int.min, count: size)
+var solutionCount:Int = 0
+
+func batchQueen() -> Bool {
+    placeQueen(y: 0)
+    return solutionCount > 0
+}
+
+func placeQueen(y:Int) {
+    if y >= size {
+        solutionCount += 1
+        xPosition[y-1] = Int.min
+        yPosition[y-1] = Int.min
+        print("====== Sucess =======")
+        return
+    }
+
+    for x in 0 ... size - 1 {
+        print("(\(x), \(y))")
+        if canPlaceQueen(destX: x, destY: y) {
+            xPosition[y] = x
+            yPosition[y] = y
+            print("<< (\(x), \(y))")
+            placeQueen(y: y + 1)
+        }
+    }
+    if y >= 1 {
+        print("release pos: \(y-1), \(y-1)/ \(xPosition[y-1]), \(yPosition[y-1])")
+        xPosition[y-1] = Int.min
+        yPosition[y-1] = Int.min
+    }
+}
+
+func canPlaceQueen(destX:Int, destY:Int) -> Bool {
+    var xPlaceable = true
+    var yPlaceable = true
+    
+    //X축 검색
+    for iter in xPosition {
+        if iter == destX {
+            xPlaceable = false
+        }
+    }
+    
+    //Y축 검색
+    for iter in yPosition {
+        if iter == destY {
+            yPlaceable = false
+        }
+    }
+    
+    //대각선 검색
+    var diagonalPlaceable = true
+    var leftDir = destX - 1
+    var rightDir = destX + 1
+    var yDir = destY - 1
+    while yDir >= 0 {
+        if xPosition[yDir] == leftDir || xPosition[yDir] == rightDir {
+            diagonalPlaceable = false
+        }
+        leftDir -= 1
+        rightDir += 1
+        yDir -= 1
+    }
+    return xPlaceable && yPlaceable && diagonalPlaceable
+}
+
+var able = batchQueen()
+print("batchAble? : \(able)")
+print("Solution Count: \(solutionCount)")
